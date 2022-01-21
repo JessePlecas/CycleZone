0f58fb2b6c8a43c51d1e7c7f1d0c2eca4350b72d
JessePlecas <jesseplecas@gmail.com>
2022-01-21 17:14:44 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+});
+
+const menu = document.getElementById("menu");
+
+menu.onclick = function(e) {
+    e.preventDefault();
+    menu.classList.toggle("open-menu");
+}
+
+const navbar = document.querySelector(".navbar");
+const hero = document.querySelector(".hero");
+
+
+const observerOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const observerOne = new IntersectionObserver
+(function(
+    entries, 
+    observerOne
+    ) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            navbar.classList.add("nav-scrolled");
+        } else {
+            navbar.classList.remove("nav-scrolled");
+        }
+    });
+}, 
+observerOneOptions);
+
+observerOne.observe(hero);
